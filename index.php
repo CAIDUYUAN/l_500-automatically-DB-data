57b5b22bdbb5169be7c08a8098452474dbfe893f
CAIDUYUAN <41244929+CAIDUYUAN@users.noreply.github.com>
2018-12-30 03:13:09 +0900

    생성할 갯수 폼

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <form action="./data_500_db.php" method="post" name='form'>
+        <h1>회원정보 생성</h1>
+        <input type="text" name='text' placeholder="생성할 갯수 입력">
+        <input type="submit" value="생성">
+    </form>
+</body>
+
+</html>
